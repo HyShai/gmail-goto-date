3af9e94d0cbf0a90c511f877a2f399c2e518951e
HyShai <HyShai@users.noreply.github.com>
2016-11-22 17:03:40 -0500

    Update popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,7 +2,7 @@
 var options = { 
 	inline: true,
 	onChange: (selectedDates, dateStr, instance) => {
-		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
 		    const current_url = new URL(tabs[0].url);
 		    //const search_string = `search/before%3A${moment(dateStr).add(1, 'day').format("YYYY-MM-DD")}%20and%20after%3A${moment(dateStr).subtract(1, 'day').format("YYYY-MM-DD")}`;
 		    const search_string = `search/before%3A${moment(dateStr).add(1, 'day').format("YYYY-MM-DD")}%20and%20after%3A${moment(dateStr).format("YYYY-MM-DD")}`;
